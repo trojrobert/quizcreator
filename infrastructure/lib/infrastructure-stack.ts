2d19330a2cfb3c7edb3946d749aecc39edd12b2f
trojrobert <trojrobert@gmail.com>
2024-09-18 17:15:01 +0200

    add lambda and gateway functionality

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,24 +1,42 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import * as lambda from "aws-cdk-lib/aws-lambda"
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as apiGateway from "aws-cdk-lib/aws-apigateway";
+import * as dotenv from "dotenv";
+import { ApiGateway } from 'aws-cdk-lib/aws-events-targets';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+dotenv.config()
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const layer = new lambda.LayerVersion(this, "BaseLayer", {
+      code: lambda.Code.fromAsset("lambda_base_layer/layer.zip"),
+      compatibleRuntimes: [lambda.Runtime.PYTHON_3_11]
+    });
+
     const apiLambda = new lambda.Function(this, "ApiFunction", {
 
       runtime: lambda.Runtime.PYTHON_3_11,
-      code: lambda.Code.fromAsset("../backend/"),
+      code: lambda.Code.fromAsset("../backend/src/"),
       handler: "main.handler",
+      layers: [layer],
+      environment: {
+        OPENAI_API_KEY: process.env.OPENAI_API_KEY ?? "",
+      }
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfrastructureQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // 
     });
+
+    const gateway = new apiGateway.RestApi(this, "RestApi", {
+      restApiName: "quiz api",
+
+    });
+
+    gateway.root.addProxy({
+      defaultIntegration: new apiGateway.LambdaIntegration(apiLambda),
+    });
+    
   }
 }
